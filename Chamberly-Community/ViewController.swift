17b23c7e125e5ff407ab7439fc7a703a3ada812b
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-03-26 00:22:34 +0530

    Added: Header

--- Chamberly-Community/ViewController.swift
+++ Chamberly-Community/ViewController.swift
@@ -1,48 +1,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private lazy var header: UILabel = {
-        let view = UILabel()
-        view.frame = CGRect(x: 0, y: 0, width: 169.14, height: 37)
-        view.text = "Communities"
-        
-        // Create gradient layer for the text
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [
-            UIColor(red: 0.478, green: 0.478, blue: 1, alpha: 1).cgColor, // #7A7AFF
-            UIColor(red: 0.973, green: 0.287, blue: 0.314, alpha: 0.7).cgColor // rgba(248, 73, 80, 0.70)
-        ]
-        gradientLayer.locations = [0, 1]
-        
-        // Apply the gradient mask to the text
-        view.layer.mask = gradientLayer
-        
-        // Set font color to black after applying the gradient mask
-        view.textColor = .black
-        
-        let parent = self.view!
-        parent.addSubview(view)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.widthAnchor.constraint(equalToConstant: 169.14).isActive = true
-        view.heightAnchor.constraint(equalToConstant: 37).isActive = true
-        view.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 0).isActive = true
-        view.topAnchor.constraint(equalTo: parent.topAnchor, constant: 20).isActive = true // Adjusted top constraint
-        
-        return view
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        home()
+        setupHorizontalRow()
     }
     
-    func home() {
-        self.view.backgroundColor = UIColor(red: 0.973, green: 0.973, blue: 1, alpha: 1)
-        self.view.addSubview(header)
+    func setupHorizontalRow() {
+        // Create a horizontal stack view
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        let label = UILabel()
+        label.text = "Communities"
+        label.font = UIFont.systemFont(ofSize: 30) // Set label font size
+        label.textAlignment = .left // Align label text to the left
+        
+        // Add label to the stack view
+        stackView.addArrangedSubview(label)
+        
+        // Create an image view
+        let imageView = UIImageView(image: UIImage(named: "pfp"))
+        
+        // Add image view to the stack view
+        stackView.addArrangedSubview(imageView)
+        
+        // Set constraints for the stack view
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20), // Adjust vertical position
+        ])
     }
 }
-
 
 #if DEBUG
 import SwiftUI
@@ -68,3 +61,4 @@ struct ViewController_Preview: PreviewProvider {
     }
 }
 #endif
+
